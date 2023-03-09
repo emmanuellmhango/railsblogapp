042fe319835acc287720b8f11285b723ce6b7820
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-09 20:51:47 +0400

    Clear errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
-  before_validation :set_default_values
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -9,20 +7,15 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
-  after_save :update_counter
+  after_create :increment_posts_counter
 
-  def update_counter
-    author.update(posts_counter: author.posts.count)
+  def increment_posts_counter
+    user.increment!(:posts_counter)
+    user.save
+    user.reload
   end
 
-  def five_recent_comments
-    comments.limit(5).order(created_at: :DESC)
-  end
-
-  private
-
-  def set_default_values
-    self.comments_counter ||= 0
-    self.likes_counter ||= 0
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
