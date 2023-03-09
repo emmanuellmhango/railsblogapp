671307e54f90cdcd10eebb339ed22504c777c847
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-09 18:12:45 +0400

    Clear errors

--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_230954) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
-    t.integer "posts_counter", default: 0
+    t.text "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
