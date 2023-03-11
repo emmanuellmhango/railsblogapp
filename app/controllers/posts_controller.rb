7db51cb1fbe7aa53aae1ef0c6f6fcb283715b52f
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-11 16:00:20 +0400

    Clear errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,10 @@ class PostsController < ApplicationController
     @posts = @user.posts.includes(:comments)
   end
 
-  # def show
-  #  @user = User.find(params[:user_id])
-  #  @post = @user.posts.find(params[:id])
-  # end
-
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.order(created_at: :desc)
+    @comment = Comment.new(post: @post)
+    # @comments = @post.comments.order(created_at: :desc)
     render :show
   end
 
