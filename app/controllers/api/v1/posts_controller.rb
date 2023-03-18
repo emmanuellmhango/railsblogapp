13986b50709d2e7d5ef35e0b3322572a2b650e8b
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-18 19:30:33 +0400

    Api documentation

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,3 @@
 class Api::V1::PostsController < ApplicationController
-  def index
-    @posts = @user.posts.order(created_at: :asc)
-    render json: @posts
-  rescue ActiveRecord::RecordNotFound
-    render json: 'Posts not found'
-  end
+
 end
